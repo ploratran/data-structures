905c3db749a02016cdc414f1ca823f1a8c8c15cc
Plora Tran <ploratran@gmail.com>
2019-05-30 16:29:07 -0700

    functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,39 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //track of queue Size
+  var queueSize = 0;
+  //act like property to track current element inside queueSize during enqueue
+  var index = 0;
+  //keep track of the first index when dequeue
+  var firstIndex = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
+    queueSize++;
+    //console.log('enqueue: ' + index + ': ' + value);
   };
 
   someInstance.dequeue = function() {
+    if(queueSize > 0){
+      //@firstVal: track of the first element in queue
+      let firstVal = storage[firstIndex];
+      delete storage[firstIndex++];
+      queueSize--;
+      // console.log(storage);
+      // console.log('firstVal: ' + firstVal);
+      return firstVal;
+    }
   };
 
   someInstance.size = function() {
+    if(queueSize < 0){
+      return 0;
+    }else{
+      return queueSize;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,34 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
+  //define size of stack. If size is 0 -> 0, if push, size++, if pop, size--
+  var stackSize = 0;
+  //act like a key of each value when adding to the stack
+  var index = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
+    stackSize++;
   };
 
   someInstance.pop = function() {
+    //console.log('Size: ' + stackSize)
+    delete storage[index];
+    index--;
+    stackSize--;
+    return storage[index];
+    //console.log(index  + ' ' + storage[index]);
   };
 
   someInstance.size = function() {
+    if(stackSize < 0){
+      return 0;
+    }else{
+      return stackSize;
+    }
   };
 
   return someInstance;

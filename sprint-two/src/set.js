4fe6e42abcdf34640c443ab7ecfab2aae6978fb4
Plora Tran <ploratran@gmail.com>
2019-12-30 15:56:30 -0800

    set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  // set._storage = null; // fix me
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  // let flag = false;
+
+  // this.storage.forEach(el => {
+  //   if (item === el) {
+  //     flag = true;
+  //   } 
+  // });
+
+  // return flag;
+  return this.storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  this.storage.shift();
 };
 
 /*

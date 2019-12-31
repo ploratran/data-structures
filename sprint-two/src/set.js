dd246668f8a5543b58c3af030126732bd8f090ea
Plora Tran <ploratran@gmail.com>
2019-12-30 16:10:04 -0800

    set with object instead of array

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,14 +1,16 @@
 var Set = function() {
   var set = Object.create(setPrototype);
   // set._storage = null; // fix me
-  set.storage = [];
+  // set.storage = [];
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this.storage.push(item);
+  // this.storage.push(item);
+  this.storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
@@ -21,11 +23,13 @@ setPrototype.contains = function(item) {
   // });
 
   // return flag;
-  return this.storage.includes(item);
+  // return this.storage.includes(item);
+  return this.storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
-  this.storage.shift();
+  // this.storage.shift();
+  delete this.storage[item];
 };
 
 /*
